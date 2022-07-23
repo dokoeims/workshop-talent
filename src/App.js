fbc3a5c4b670826faa4b6058b641bf3d6db17eb1
Eduardo Mondragon <eduardo@cl1cky.com>
2022-07-22 20:29:25 -0500

    fetching data from bitso api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import { isEmpty } from 'lodash'
+
+import { fetchData, fetchOrderBookData } from './api'
+import { API } from './constants'
+import { getBooksOptions } from './helpers'
+import { styles } from './styles'
+
+import { Box, FormControl, InputLabel, Select, MenuItem } from '@mui/material'
 
 function App() {
+  const [booksOptions, setBooksOptions] = useState([])
+  const [selectedBook, setSelectedBook] = useState('')
+
+  useEffect(() => {
+    async function fetchBooksData(){
+      const data = await fetchData(`${API.ROOT}${API.ENDPOINTS.AVAILABLE_BOOKS}`)
+
+      if(!isEmpty(data))
+        setBooksOptions(getBooksOptions(data))
+    }
+    
+    fetchBooksData()
+    
+  }, [])
+
+  useEffect(() => {
+    if(booksOptions.length > 0)
+      setSelectedBook(booksOptions[0].value)
+  }, [booksOptions])
+  
+  useEffect(() => {
+    async function fetchOrderBook(){
+      const data = await fetchOrderBookData(`${API.ROOT}${API.ENDPOINTS.ORDER_BOOK}`, selectedBook)
+      console.log('Book: ', data)
+    }
+
+    if(selectedBook)
+      fetchOrderBook()
+  
+  }, [selectedBook])
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Box sx={styles.container}>
+      <FormControl>
+        <InputLabel id="book">Book</InputLabel>
+        <Select
+          labelId="book"
+          id="book-select"
+          value={selectedBook}
+          label="Books"
+          onChange={event => setSelectedBook(event.target.value)}
+          sx={styles.select}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          {
+            booksOptions.map(book => <MenuItem key={book.value} value={book.value}>{book.label}</MenuItem>)
+          }
+        </Select>
+      </FormControl>
+      
+    </Box>
+  )
 }
 
-export default App;
+export default App
---
+++ src/styles.js
@@ -0,0 +1,8 @@
+export const styles = {
+    container: {
+        padding: '2rem'
+    },
+    select: {
+        width: '16rem'
+    }
+}
